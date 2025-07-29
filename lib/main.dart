3b051f84d1849dcaf51def7a6a527673fd638cdc
Mahmoud Mohamed <ma5975986@gmail.com>
2025-07-29 08:52:05 +0300

    Update NDK version, initialize map style, and add map style JSON asset

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -12,38 +16,52 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+  String _mapStyle = '';
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  @override
+  void initState() {
+    super.initState();
+    _loadMapStyle();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: LatLng(30.0444, 31.2357),
+            zoom: 14.4746,
+          ),
+          style: _mapStyle,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  void _loadMapStyle() async {
+    final style = await DefaultAssetBundle.of(
+      context,
+    ).loadString('assets/map_style.json');
+    setState(() {
+      _mapStyle = style;
+    });
   }
 }
+
+// To navigate to a specific location, you can use the following method:
+// void goToLocation(LatLng location) async {
+//   final GoogleMapController controller = await _controller.future;
+//   controller.animateCamera(
+//     CameraUpdate.newCameraPosition(
+//       CameraPosition(
+//         target: location,
+//         zoom: 14.4746,
+//       ),
+//     ),
+//   );
+// }
